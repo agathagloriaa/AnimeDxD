fae0dff72532aab9539bec99b358a5a5ee0ec069
naufaldimas <naufaldimasazizan@gmail.com>
2025-08-16 23:17:55 +0700

    update onclick review in detail page

--- app/src/main/java/com/example/animedxd/DetailActivity.java
+++ app/src/main/java/com/example/animedxd/DetailActivity.java
@@ -1,13 +1,23 @@
 package com.example.animedxd;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView tvErrorReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,46 @@ public class DetailActivity extends AppCompatActivity {
 
         ImageButton btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(v -> finish());
+
+        Button btnReview = findViewById(R.id.btn_review);
+        btnReview.setOnClickListener(v -> {
+            // Buat dialog
+            Dialog dialog = new Dialog(DetailActivity.this);
+            dialog.setContentView(R.layout.dialog_review);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // biar rounded
+
+            EditText inputReview = dialog.findViewById(R.id.input_review);
+            TextView tvErrorReview = dialog.findViewById(R.id.tvErrorReview); // <-- ambil dari dialog!
+            Button btnSubmit = dialog.findViewById(R.id.btn_submit_review);
+
+            btnSubmit.setOnClickListener(view -> {
+                String review = inputReview.getText().toString().trim();
+                boolean isValid = true;
+
+                // --- Review Validation ---
+                if (TextUtils.isEmpty(review)) {
+                    tvErrorReview.setText("Review is required");
+                    tvErrorReview.setVisibility(View.VISIBLE);
+                    isValid = false;
+                } else {
+                    tvErrorReview.setVisibility(View.INVISIBLE);
+                }
+
+                if (!isValid) return;
+
+
+                dialog.dismiss(); // tutup setelah sukses submit
+            });
+
+            dialog.show();
+            dialog.getWindow().setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT, // lebar full
+                    ViewGroup.LayoutParams.WRAP_CONTENT  // tinggi menyesuaikan
+            );
+        });
+
+
     }
+
 
 }
